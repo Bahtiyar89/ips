6fc54c85e2111fbd526449e151f074edb2895e45
Bahtiyar89 <bahtiyar1989@gmail.com>
2024-06-11 16:47:47 +0500

    navigation added

--- App.js
+++ App.js
@@ -11,21 +11,70 @@ import {
   Modal,
 } from 'react-native';
 import {StatusBar} from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
+  function Feed({navigation}) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Feed</Text>
+        <Button
+          onPress={() => navigation.navigate('Home')}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Feed"
+        />
+      </View>
+    );
+  }
+  function Article({navigation}) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Article</Text>
+        <Button
+          onPress={() => navigation.navigate('Home')}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Feed"
+        />
+      </View>
+    );
+  }
+  function HomeScreen({navigation}) {
+    return (
+      <Drawer.Navigator>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
+    );
+  }
+  function Home1Screen({navigation}) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Home Screen1</Text>
+        <Button
+          onPress={() => navigation.navigate('Home')}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <SafeAreaView>
-      <Text
-        style={{
-          fontSize: 29,
-          fontWeight: '400',
-          color: 'black',
-          textAlign: 'center',
-        }}>
-        Hello World !
-      </Text>
-      <Text> 1233456</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home1" component={Home1Screen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
